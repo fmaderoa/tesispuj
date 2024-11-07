878e439951fcc25ca4a0406c83f1e00625b23b62
fmaderoa <45087899+fmaderoa@users.noreply.github.com>
2024-11-07 08:20:15 -0500

    Delete Proyectos2024 directory

--- Proyectos2024/app/src/main/java/com/example/marketplacepuj/ui/producto/data/entities/Producto.kt
+++
@@ -1,9 +0,0 @@
-package com.example.marketplacepuj.ui.producto.data.entities
-
-data class Producto(
-    val id: String,
-    val titulo: String,
-    val descripcion: String,
-    val precio: Double,
-    val imagen: String
-)
